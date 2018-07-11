99659de90c4b057a17dc24fff8e88eb714aa17fc
Angie Kassab <anasab12@gmail.com>
2018-07-11 17:06:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
-def reverse_each_word(sentence)
-  sentence.split.collect { |word| word.reverse }.join(" ")
+def reverse_each_word(string_with_word_reversed)
+  split_sentence = sentence.split
+  split_sentence.collect do |word|
+    word.reverse
+	end
+  .join(" ")
 end
