a51647baec8ee5682aa3e14fd36179b933f3a095
Angie Kassab <anasab12@gmail.com>
2018-07-11 17:03:36 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,10 +5,3 @@ def reverse_each_word(string_with_word_reversed)
 	end
   .join(" ")
 end
-
-
-
-#reverse_each_word
-  reverses all the words in a string without reversing the order of the words (FAILED - 1)
-  reverses all the words in another string without reversing the order of the words (FAILED - 2)
-  uses collect (FAILED - 3)
