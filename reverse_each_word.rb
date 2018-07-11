b0695364b95be49dbd5d5c541b6dc3be023bc228
Angie Kassab <anasab12@gmail.com>
2018-07-11 17:09:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,16 @@ def reverse_each_word(sentence)
 	end
   .join(" ")
 end
+
+=begin
+This is a shorter method below:
+
+def reverse_each_word(sentence)
+  split_sentence = sentence.split
+  split_sentence.collect do |word|
+    word.reverse
+	end
+  .join(" ")
+end
+
+=end 
