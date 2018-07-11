941dad47b74018a44f63ddae44b71149e5697485
Angie Kassab <anasab12@gmail.com>
2018-07-11 17:12:24 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,13 @@
 def reverse_each_word(sentence)
   split_sentence = sentence.split
+  split_sentence.each do |word|
+    word.reverse
+	end
+  .join(" ")
+end
+
+def reverse_each_word(sentence)
+  split_sentence = sentence.split
   split_sentence.collect do |word|
     word.reverse
 	end
